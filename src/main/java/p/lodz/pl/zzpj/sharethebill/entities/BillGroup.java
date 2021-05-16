124a08c8a88900f43402632bc2969f0162209c10
badwolf4 <badwolf43200@gmail.com>
2021-05-16 23:00:21 +0300

    fixing id sequences

--- src/main/java/p/lodz/pl/zzpj/sharethebill/entities/BillGroup.java
+++ src/main/java/p/lodz/pl/zzpj/sharethebill/entities/BillGroup.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 public class BillGroup {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
