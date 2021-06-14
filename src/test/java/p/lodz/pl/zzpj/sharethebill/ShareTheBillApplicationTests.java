3c34d30e92391cafb30dfb9e3d56561a534fb875
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-06-14 14:15:01 +0200

    Test Properties

--- src/test/java/p/lodz/pl/zzpj/sharethebill/ShareTheBillApplicationTests.java
+++ src/test/java/p/lodz/pl/zzpj/sharethebill/ShareTheBillApplicationTests.java
@@ -2,10 +2,13 @@ package p.lodz.pl.zzpj.sharethebill;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import p.lodz.pl.zzpj.sharethebill.services.CurrencyService;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-integration.properties")
 class ShareTheBillApplicationTests {
 
     @Test
